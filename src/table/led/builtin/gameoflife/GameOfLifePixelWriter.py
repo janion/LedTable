9bbf9bfb24e18f111e1aaa38a48865fe673225a4
Janion <Janion@192.168.2.3>
2017-05-24 23:19:05 +0100

    Changed snake to fade along the colour wheel along the length of it's body

--- src/table/led/builtin/gameoflife/GameOfLifePixelWriter.py
+++ src/table/led/builtin/gameoflife/GameOfLifePixelWriter.py
@@ -14,21 +14,6 @@ class PixelWriter(PixelWriter2D):
         self.lastIncrement = None
         self.timeTick = self.STEP_DURATION
         self.gameOfLife = GameOfLife(ledCountX, ledCountY)
-        # self.gameOfLife.setStartCondition([(4, 0), (4, 1), (4, 2), (4, 3), (4, 4),
-        #                                    (4, 5), (4, 6), (4, 7), (4, 8), (4, 9),
-        #
-        #                                    (0, 4), (1, 4), (2, 4), (3, 4), (5, 4),
-        #                                    (6, 4), (7, 4), (8, 4), (9, 4),
-        #
-        #                                    (0, 7), (1, 7), (2, 7), (3, 7), (5, 7),
-        #                                    (6, 7), (7, 7), (8, 7), (9, 7),
-        #
-        #                                    (7, 0), (7, 1), (7, 2), (7, 3),
-        #                                    (8, 5), (8, 6), (8, 8), (8, 9),
-        #
-        #                                    (1, 0), (1, 1), (2, 2), (3, 3), (4, 4),
-        #                                    (5, 5), (6, 6), (7, 7), (8, 8), (9, 9)
-        #                                    ])
         self.gameOfLife.setStartCondition([(1, 0), (1, 1), (2, 2), (3, 3), (4, 4),
                                            (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (8, 9)
                                            ])
